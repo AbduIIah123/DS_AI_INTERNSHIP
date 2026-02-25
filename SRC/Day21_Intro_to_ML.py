6ed079a03551a9cd29720c89221e83ee8c01a466
abdullah <abdullahabdxxy@gmail.com>
2026-02-25 12:28:12 +0530

    day21

---
+++ SRC/Day21_Intro_to_ML.py
@@ -0,0 +1,26 @@
+# Task 1
+
+# Machine Learning:
+# Machine Learning is a feild in artificial intelligence. Machine Learning can learn and
+# study patterns from data, and perform tasks without human intervention.
+# Machine Learning allows the computer to make predictions and decisions
+# without being manually programmed for every task.
+# Machine Learning allows systems to learn from mistakes and
+# experience, similar to how humans learn.
+
+# Task 2
+
+# Supervised Learning:
+# Supervised Learning is a machine learning paradigm where data has labels
+# where models are trained using labeled data, meaning input data is paired
+# with corresponding, known outputs.
+
+# Unsupervised Learning:
+# Unsupervised Learning is a machine learning paradigm where has no data
+# has labels where models analyze hidden clusters, structures, or
+# relationships without human interventions.
+
+# Reinforced Learning:
+# It is a machine learning technique where the model learns to make
+# optimal, sequential decisions by interacting with an envirnment
+# through trial and error.
